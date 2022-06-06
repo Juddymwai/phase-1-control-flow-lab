8256ace0d65a1272bbcf4485cacf1b4e0ed1211a
Judy Mwai <juddiemukami@gmail.com>
2022-06-06 22:09:40 +0300

    commit tree 9cd82c90c9b6cfbdc6b8ad7200a582bd1cf03791

--- index.js
+++ index.js
@@ -1,11 +1,53 @@
-function scuberGreetingForFeet(){
+
+let feet = 300;
+
+function scuberGreetingForFeet(feet){
   // Write your code here!
+ 
+  if (feet <=400){
+    return 'This one is on me!';
+    
+  }
+
+
+  else if (feet > 2000 && feet <= 2500){
+
+    return "I will gladly take your thirty bucks.";
+
+   
+  }
+  else {
+    return "No can do.";
+
+  }
 }
 
-function ternaryCheckCity(){
+let city;
+function ternaryCheckCity(city){
   // Write your code here!
-}
+  if (city=='NYC'){
+    return 'Ok, sounds good.';
+  }
+  else if (city!=='NYC') {
+    return 'No go.';
 
-function switchOnCharmFromTip(){
+  }
+  
+}
+let tip;
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  if (tip == 'generous'){
+    return 'Thank you so much.';
+  }
+  else if (tip === 'not as generous'){
+    return 'Thank you.';
+
+  }
+  else if ('Thank you.') {
+    return 'Bye.';
+
+  }
+  
+  
 }
